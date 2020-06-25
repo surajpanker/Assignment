b8ba335161905d03ce8d910aaaa1397a8fae2856
suraj82 <suraj82@bitbucket.org>
2020-06-25 16:35:31 +0530

    right

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb+srv://root:root@cluster0-26bbm.mongodb.net/Assign?retryWrites=true&w=majority
-");
+mongoose.connect("mongodb+srv://root:root@cluster0-26bbm.mongodb.net/Assign?retryWrites=true&w=majority");
 
 module.exports = {mongoose};
