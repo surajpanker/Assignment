9252cce441284ce00b27ab6a5f7c2aecbefd700b
suraj82 <suraj82@bitbucket.org>
2020-06-25 16:21:43 +0530

    add mongo link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://127.0.0.1:27017");
+mongoose.connect("mongodb+srv://root:root@cluster0-26bbm.mongodb.net/Assign?retryWrites=true&w=majority
+");
 
 module.exports = {mongoose};
